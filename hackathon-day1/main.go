c7b927929816d0ba3158e37394a8c5739919834b
goemaikwu <emaikwungbede@gmail.com>
2026-03-26 23:26:08 +0100

    .

---
+++ hackathon-day1/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+)
+
+func add(a, b int) int {
+	return a + b
+}
+
+func sub(a, b int) int {
+	return a - b
+}
+
+func mul(a, b int) int {
+	return a * b
+}
+
+func div(a, b int) int{
+	return a / b
+}
+
+func main() {
+	for {
+	var a , b int
+	fmt.Println("input first integer")
+	fmt.Scanln(&a)
+
+
+	fmt.Println("input second integer")
+	fmt.Scanln(&b)
+
+
+	var operation string
+
+		fmt.Println("operation = add, sub, mul, div, quit, help, cap")
+		fmt.Scanln(&operation)
+
+		switch operation {
+		case "add":
+			fmt.Println(add(a, b))
+			continue
+
+		case "sub":
+			fmt.Println(sub(a, b))
+			continue
+		
+		case "mul":
+			fmt.Println(mul(a, b))
+			continue
+
+		case "div":
+			fmt.Println(div(a, b))
+			continue
+
+		case "quit":
+			fmt.Println("Thank you for testing my code")
+			return
+
+		case "help":
+			fmt.Println("supported commands = add, sub, mul, div, quit, help")
+
+		case "cap":
+			fmt.Println("not a valid expression, type help for valid expressions")
+
+		default:
+			fmt.Println("Invalid expression")
+		}
+		
+	}
+	
+}
