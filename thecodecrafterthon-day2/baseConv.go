97055f5d117dc446f3fcf0378926a56edf1b9271
gooemaikwu <emaikwungbede@gmail.com>
2026-03-27 18:20:02 +0100

    .

--- thecodecrafterthon-day2/baseConv.go
+++ thecodecrafterthon-day2/baseConv.go
@@ -6,23 +6,23 @@ import (
 )
 
 func ConvToDec(num string, base int) (int64, error) {
-	decimal, err := strconv.ParseInt(num, 10, 64)
+	decimal, err := strconv.ParseInt(num, base, 64)
 	if err != nil {
 		fmt.Println("Value is not a decimal number, kindly input a decimal number")
 	}
 	return decimal, err
 }
 
-func ConvToHex(num string) (int64, error) {
-	hex, err := strconv.ParseInt(num, 16, 64)
+func ConvToHex(num string, base int) (int64, error) {
+	hex, err := strconv.ParseInt(num, base, 64)
 	if err != nil {
 		fmt.Println("Value is not a hexadecimal number, kindly input a decimal number")
 	}
 	return hex, err
 }
 
-func ConvToBin(num string) (int64, error) {
-	bin, err := strconv.ParseInt(num, 2, 64)
+func ConvToBin(num string, base int) (int64, error) {
+	bin, err := strconv.ParseInt(num, base, 64)
 	if err != nil {
 		fmt.Println("Value is not a binary number, kindly input a decimal number")
 	}
@@ -43,17 +43,21 @@ func main() {
 		fmt.Println(colour("choose operation (bin, dec, hex, quit)", "\033[36m"))
 		fmt.Scanln(&operation)
 
+		var base int
+		fmt.Println("What number base is the input")
+		fmt.Scan(&base)
+
 		switch operation {
 		case "bin":
-			fmt.Println(ConvToBin(num))
+			fmt.Println(ConvToBin(num, base))
 			continue
 
 		case "dec":
-			fmt.Println(ConvToDec(num, 64))
+			fmt.Println(ConvToDec(num, base))
 			continue
 
 		case "hex":
-			fmt.Println(ConvToHex(num))
+			fmt.Println(ConvToHex(num, base))
 			continue
 
 		case "quit":
@@ -61,7 +65,7 @@ func main() {
 			return
 
 		default:
-			fmt.Println("Not a valid conversion, try again")
+			fmt.Println("Not a valid conversion, try again", "\033[38m")
 			fmt.Println("valid conversions are: bin, dec and hex")
 		}
 	}
