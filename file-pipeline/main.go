f75d20b254f7c34f8f0be6e98bad1971563aa01c
kvngragnar <emaikwungbede@gmail.com>
2026-03-31 21:35:33 +0100

    .

--- file-pipeline/main.go
+++ file-pipeline/main.go
@@ -1,10 +1,23 @@
-// CodeCrafters — Operation Gopher Protocol
-// Module: File Pipeline
-// Author: [Godwin Emaikwu]
-// Squad:  [The Benchmarkers]
+ // CodeCrafters — Operation Gopher Protocol
+       // Module: File Pipeline
+       // Author: [Godwin Emaikwu]
+       // Squad:  [The Benchmarkers]
+
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
+func RemToDo (s string) string {
+	words := strings.Fields(s)
+	for i = 0; i < len(words); i++ {
+		if words[i] = "TODO" {
+			words[i] = "ACTION"
+		}
+	}
+	return strings.Join(words," ")
 }
